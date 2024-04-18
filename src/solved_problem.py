7a1f2e78599a788d771ba977b543787a1190a2d5
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-04-18 18:02:44 +0300

    Added dataclass for solved problems

---
+++ src/solved_problem.py
@@ -0,0 +1,18 @@
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class SolvedProblem:
+    link: str = None
+    name: str = None
+    points: str = None
+    difficulty: str = None
+    code: List[str] = None
+
+    def __repr__(self) -> str:
+        return f'''
+Problem {self.name}
+link {self.link}
+points {self.points}
+Difficulty {self.difficulty}'''
