61ae006ec512e15a9d422f5df490d2b665eb4e0f
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-04-21 15:40:46 +0300

    Added --directory argument to parser

--- test/test_argument_parser.py
+++ test/test_argument_parser.py
@@ -2,12 +2,14 @@ from src.argument_parser import parse_arguments
 
 
 def test_parse_short_arguments():
-    parser = parse_arguments(['-u', 'my_username', '-p', 'my_password'])
+    parser = parse_arguments(['-u', 'my_username', '-p', 'my_password', '-d', '../../Solutions'])
     assert parser.user == 'my_username'
     assert parser.password == 'my_password'
+    assert parser.directory == '../../Solutions'
 
 
 def test_long_arguments():
-    parser = parse_arguments(['--user', 'my_username', '--password', 'my_password'])
+    parser = parse_arguments(['--user', 'my_username', '--password', 'my_password', '--directory', '../../Solutions'])
     assert parser.user == 'my_username'
     assert parser.password == 'my_password'
+    assert parser.directory == '../../Solutions'
