37b6c6b9fede3a95df3394cc924f231251d847c2
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-04-26 07:55:39 +0300

    Updates: -Created ProblemStatus IntEnum -Combined SolvedProblem file_name and code into a dict -Added function to_dict

--- src/solved_problem.py
+++ src/solved_problem.py
@@ -1,26 +1,38 @@
+from typing import Dict
 from pathlib import Path
+from enum import IntEnum
 from dataclasses import dataclass
+
+
+class ProblemStatus(IntEnum):
+    CODE_FOUND = 1
+    UPDATE = 0
+    CODE_NOT_FOUND = -1
 
 
 @dataclass
 class SolvedProblem:
     link: str = None
     name: str = None
-    file_name: str = None
     points: str = None
     difficulty: str = None
-    code = []
+    status: int = ProblemStatus.CODE_NOT_FOUND
+    filename_code_dict = {}
 
     def write_to_file(self, directory: Path) -> None:
         # TODO: Expand to write multiple files if different languages found
-        with open(directory / self.difficulty / self.file_name, 'w') as file:
-            file.write(self.code[0])
+        for filename in self.filename_code_dict:
+            with open(directory / self.difficulty / filename, 'w') as file:
+                file.write(self.filename_code_dict[filename])
+
+    def to_dict(self) -> Dict:
+        return {'Name': self.name, 'Difficulty': self.difficulty, 'Status': self.status.value}
 
     def __repr__(self) -> str:
         return \
 f'''Problem {self.name}
-filename {self.file_name}
-link {self.link}
-points {self.points}
+Link {self.link}
+Points {self.points}
 Difficulty {self.difficulty}
-{self.code}'''
+Status {self.status._name_}
+{self.filename_code_dict}'''
