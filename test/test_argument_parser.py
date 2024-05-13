eb33693a47fbfc3749dfd34037f973a335bb518e
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-05-13 16:24:39 +0300

    Added argument --update: -If this argument is given, KTG only checks poblems with status 0 in the .csv file

--- test/test_argument_parser.py
+++ test/test_argument_parser.py
@@ -6,10 +6,12 @@ def test_parse_short_arguments():
     assert parser.user == 'my_username'
     assert parser.password == 'my_password'
     assert parser.directory == '../../Solutions'
+    assert parser.update is False
 
 
 def test_long_arguments():
-    parser = parse_arguments(['--user', 'my_username', '--password', 'my_password', '--directory', '../../Solutions'])
+    parser = parse_arguments(['--user', 'my_username', '--password', 'my_password', '--directory', '../../Solutions', '--update'])
     assert parser.user == 'my_username'
     assert parser.password == 'my_password'
     assert parser.directory == '../../Solutions'
+    assert parser.update is True
