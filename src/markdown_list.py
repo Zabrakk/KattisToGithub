c5c4791ff68091a2c284ab87a712ec6b8f8203c0
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-05-19 16:06:36 +0300

    Documentation

--- src/markdown_list.py
+++ src/markdown_list.py
@@ -7,6 +7,13 @@ from src.solved_problem import SolvedProblem, ProblemStatus
 
 
 class MarkdownList:
+    """
+    MarkdownList is used to automatically create / update the README.md with a list of solved problems.
+
+    Parameters:
+    - directory: A Path object pointing to the location where the README.md file should be created.
+    - solved_problems: A list of SolvedProblem objects. The list is created based on these.
+    """
     def __init__(self, directory: Path, solved_problems: List[SolvedProblem]) -> None:
         self.__filepath = directory / 'README.md'
         self.__solved_problems = deepcopy(solved_problems)
@@ -25,6 +32,9 @@ class MarkdownList:
 
     @property
     def should_add_and_commit(self) -> bool:
+        """
+        If true, the created README.md file should be added and commited with Git.
+        """
         return self.__new_contents != self.__original_contents
 
     @property
@@ -32,6 +42,13 @@ class MarkdownList:
         return self.__filepath
 
     def _load_existing_README_contents(self) -> None:
+        """
+        Loads the contents of a README.md which might already exist.
+        The contents is then appended with KattisToGithub info.
+
+        Returns:
+        - None
+        """
         if os.path.exists(self.__filepath):
             with open(self.__filepath, 'r') as md_file:
                 self.__original_contents = md_file.readlines()
@@ -48,10 +65,24 @@ class MarkdownList:
         return new_contents
 
     def _sort_solved_problems_by_difficulty(self) -> None:
+        """
+        Sorts the MarkdownList's SolvedProblem list by difficulty.
+        Once sorted, Hard difficulty SolvedProblems are first in the list, Easy problems are at the end, and Medium in the middle.
+
+        Returns:
+        - None
+        """
         order = ['Hard', 'Medium', 'Easy']
         self.__solved_problems.sort(key=lambda sp: order.index(sp.difficulty))
 
     def _create_solved_problem_list(self) -> List[str]:
+        """
+        Create a list of strings based on the SolvedProblems which don't have the status CODE_NOT_FOUND. The strings follow the format:
+        |[Problem name](link to problem) | Problem difficulty | [Solution programming language](link to solution)|
+
+        Returns:
+        - List[str]: Markdown list of SolvedProblems
+        """
         return [
             f'|[{sp.name}]({sp.problem_link})|{sp.difficulty}|' + self.__create_solutions_tab_content_for_solved_problem(sp) + '\n' \
             for sp in self.__solved_problems if sp.status != ProblemStatus.CODE_NOT_FOUND
@@ -61,10 +92,25 @@ class MarkdownList:
         return ' '.join([f'[{language}](Solutions/{filename})' for filename, language in solved_problem.filename_language_dict.items()])
 
     def _save_contents_to_README(self, contents: List[str]) -> None:
+        """
+        Saves the contents, given as a paramter, into README.md.
+
+        Parameters:
+        - contents: List of strings which will be written into the README.md file
+
+        Returns:
+        - None
+        """
         with open(self.__filepath, 'w') as md_file:
             md_file.writelines(contents)
 
-    def create(self):
+    def create(self) -> None:
+        """
+        Creates and saves the README.md made by KattisToGithub
+
+        Returns:
+        - None
+        """
         self._load_existing_README_contents()
         self._sort_solved_problems_by_difficulty()
         self.__new_contents += self._create_solved_problem_list()
