785fe8fa81a042c5a5ffe48d261a1bc7c4ecbc0d
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-05-19 14:07:00 +0300

    Refactoring

--- src/constants.py
+++ src/constants.py
@@ -3,4 +3,6 @@ LOGIN_URL = 'https://open.kattis.com/login/email'
 CSV_FIELD_NAMES = ['Name', 'Difficulty', 'Status', 'ProblemLink', 'SubmissionsLink', 'Solutions']
 
 README_LIST_TITLE = '## Solved Problems\n'
+README_LIST_COLUMN_TITLES = '|Problem|Difficulty|Solutions|\n'
+README_LIST_POSITIONING = '|:-|:-|:-|\n'
 KTG_AD = '<sub><i>Created with [KattisToGithub](https://github.com/Zabrakk/KattisToGithub)</i></sub>\n'
